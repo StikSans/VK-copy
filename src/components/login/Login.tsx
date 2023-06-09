e4a8df9c4334b47a9284277005a86ab0b17e7005
StikSans <113697477+StikSans@users.noreply.github.com>
2023-06-09 17:46:45 +0400

    Update Login.tsx

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -14,14 +14,14 @@ const Login = () => {
                 </div>
                 <button className={styles.loginBut}>Войти</button>
             </form>
-            <div className={styles.qr_code}>
+<!--             <div className={styles.qr_code}>
                 <div className={'w-[76px] h-[76px] bg-[#FFFFFF] rounded-[8px]'}></div>
                 <div className={styles.text_qr_code}>
                     <p className={'text-[14px]'}>Быстрый вход по QR‑коду</p>
                     <p className={'text-[13px] text-[#828282]'}>Наведите камеру телефона</p>
                     <span className={'text-[14px] text-[#71AAEB] flex items-start pt-[8px]'}>Подробнее</span>
                 </div>
-            </div>
+            </div> -->
         </section>
     )
 }
